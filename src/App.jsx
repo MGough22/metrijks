35c36ab5465702120b6fe23df9f03bc99152746a
MGough22 <gaivsvalerivs@gmail.com>
2025-06-17 20:06:54 +0100

    adds dismissable banner alert for landscape suggestion

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,29 @@ import SearchBar from "./components/SearchBar";
 import ArtworkList from "./components/ArtworkList";
 import { ArtworkDetail } from "./components/ArtworkDetail";
 import { Exhibition } from "./components/Exhibition";
+import { LandscapeNotice } from "./components/LandscapeNotice";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
+    <>
+      <LandscapeNotice />
+      <div className="min-h-screen flex flex-col">
+        <Header />
 
-      <main className="flex-grow">
-        <SearchBar setSearchTerm={setSearchTerm} />
-        <Routes>
-          <Route path="/exhibition" element={<Exhibition />} />
-          <Route path="/" element={<ArtworkList searchTerm={searchTerm} />} />
-          <Route path="/artworks/:source/:id" element={<ArtworkDetail />} />
-        </Routes>
-      </main>
+        <main className="flex-grow">
+          <SearchBar setSearchTerm={setSearchTerm} />
+          <Routes>
+            <Route path="/exhibition" element={<Exhibition />} />
+            <Route path="/" element={<ArtworkList searchTerm={searchTerm} />} />
+            <Route path="/artworks/:source/:id" element={<ArtworkDetail />} />
+          </Routes>
+        </main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </>
   );
 }
 
